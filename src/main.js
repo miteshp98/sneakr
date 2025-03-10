7194fe740a4905a70e2539aefbb062687a9a1372
miteshp98 <miteshpanchal3d@gmail.com>
2025-03-10 21:53:40 +0530

    add new scripts

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 import toggleNav from "./js/components/navbarToggle";
 import toggleCart from "./js/components/toggleCart";
+import { featuredProducts } from "./js/components/featuredProducts";
+import toggleCategoryDropdown from "./js/components/productFilterDropdown";
+import { loadProducts } from "./js/components/productList";
+
+const displayedFeaturedProducts = featuredProducts();
+const manageCategoryDropdown = toggleCategoryDropdown();
+loadProducts();
