5dfeb9c1fc1fd5651e21738c8e9953e90318ad37
miteshp98 <miteshpanchal3d@gmail.com>
2025-03-10 21:51:28 +0530

    add product list fetch products  & show on base of category

---
+++ src/js/components/productList.js
@@ -0,0 +1,55 @@
+import { productsEntries } from "../services/contentfulClient";
+import { productLoader } from "../services/skeletonLoader";
+import { productSkeleton, productErrorHtml, productCard } from "../utils/markupGenerator";
+
+export function checkCategory() {
+    const categoryOption = document.querySelector(".products__custom-trigger");
+
+    if (!categoryOption) return;
+
+    const category = categoryOption.textContent.toLowerCase().replaceAll(" ", "");
+
+    switch (category) {
+        case "men":
+            return category;
+        case "women":
+            return category;
+        default:
+            return;
+    }
+}
+
+export async function loadProducts() {
+    const productItemList = document.querySelector(".products__item-list");
+    const selectedCategory = checkCategory();
+
+    if (!productItemList) return;
+
+    productLoader(productItemList, 6, productSkeleton);
+
+    try {
+        const data = await productsEntries(0, 9, "sneakrProducts", "sys.createdAt");
+
+        productItemList.innerHTML = filterProductsByCategory(data.items, selectedCategory);
+    } catch (error) {
+        console.error(`error fetching products ${error}`);
+        productItemList.innerHTML = productErrorHtml("Failed to load Products");
+    }
+}
+
+function filterProductsByCategory(products, category) {
+    return products
+        .filter(({ fields }) => {
+            if (!category) return true;
+            return fields.category.toLowerCase() === category;
+        })
+        .map(({ fields: { productName, slug, productImage, price, currency, category } }) => {
+            const {
+                description: altText,
+                file: { url: imageUrl },
+            } = productImage.fields;
+
+            return productCard(imageUrl, altText, productName, price, currency, category, slug);
+        })
+        .join("");
+}
