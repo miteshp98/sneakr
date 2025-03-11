160e6af2977704cd794d305cdf41f41f6340964f
miteshp98 <miteshpanchal3d@gmail.com>
2025-03-11 15:15:39 +0530

    add product view logic on slug

---
+++ src/js/page/productView.js
@@ -0,0 +1,80 @@
+import { productViewLoader, productViewHtml, productViewSEOHtml } from "../utils/markupGenerator";
+import { client } from "../utils/contentfulUtils";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+
+const urlParams = new URLSearchParams(window.location.search);
+const productSlug = urlParams.get("slug");
+
+function redirectIfInvalidSlug() {
+    if (!productSlug) {
+        window.location = "page-not-found.html";
+        return;
+    }
+}
+redirectIfInvalidSlug();
+
+export async function loadProductDetails() {
+    const productContainer = document.querySelector(".productview__container");
+
+    if (!productContainer) return;
+
+    productContainer.innerHTML = productViewLoader();
+
+    try {
+        const productEntries = await client.getEntries();
+
+        const selectedProduct = productEntries.items.find((entry) => {
+            return entry.fields.slug === productSlug;
+        });
+
+        if (!selectedProduct) {
+            throw new Error(`Product no found`);
+        }
+
+        const {
+            productName,
+            slug,
+            shortDescription,
+            description,
+            price,
+            discountedPrice,
+            currency,
+            productImage: {
+                fields: {
+                    description: imageDescription,
+                    file: { url: productImageUrl },
+                },
+            },
+            seoTitle,
+            metaDescription,
+            metaTags,
+            shoesType,
+        } = selectedProduct.fields;
+
+        productViewSEOHtml(seoTitle, metaDescription, metaTags);
+
+        productContainer.innerHTML = productViewHtml(
+            productImageUrl,
+            imageDescription,
+            formatProductDescription(description),
+            shoesType,
+            productName,
+            price,
+            discountedPrice
+        );
+    } catch (error) {
+        console.error("Error fetching products:", error.message || error);
+        window.location = "page-not-found.html";
+    }
+}
+
+loadProductDetails();
+
+function formatProductDescription(textData) {
+    if (!textData) return "";
+
+    return textData
+        .split("\n")
+        .map((line) => `<p>${line}</p>`)
+        .join("");
+}
